8a05f62328d864b509dc478f86009a2cc09e98f7
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-20 21:11:06 +0530

    Add records and maps

---
+++ src/recordsAndMaps.ts
@@ -0,0 +1,11 @@
+type User = Record<string, { age: number; name: string }>; //TS specific concept
+
+const user: User = {
+  user1: { age: 21, name: "Rajneesh" },
+};
+
+console.log(user);
+
+const map = new Map(); //JS concept
+const user1 = map.set("Rajneesh", { age: 21, name: "Rajneesh" });
+console.log(user1);
